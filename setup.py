28a46538064cdfdb9687b1b8eafaa4015797ee86
Vijay6923 <kumarviju2468@gmail.com>
2025-05-06 12:44:54 +0530

    setup and packages

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from typing import List
+HYPHEN_E_DOT='-e .'
+
+def get_requirements(file_path: str) -> List[str]:
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements if req.strip() and not req.startswith("#")]
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Vijay',
+    author_email='kumarviju2468@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
